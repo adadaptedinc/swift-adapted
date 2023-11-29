565a3392572648ca416300d418e8a0060aad1a14
Brett Clifton <bclifton@adadapted.com>
2023-11-29 11:20:45 -0500

    feat: basic ui elements

--- SwiftAdapted/SwiftAdapted/AppDelegate.swift
+++ SwiftAdapted/SwiftAdapted/AppDelegate.swift
@@ -6,12 +6,11 @@
 //
 
 import UIKit
+import adadapted_swift_sdk
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        //TODO sdk hookup
         
         return true
     }
