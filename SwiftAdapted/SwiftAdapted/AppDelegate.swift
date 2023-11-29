85ff2a413decfac908ce73ba8d4b9839f1506c66
Brett Clifton <bclifton@adadapted.com>
2023-11-29 15:27:35 -0500

    feat:package settings

--- SwiftAdapted/SwiftAdapted/AppDelegate.swift
+++ SwiftAdapted/SwiftAdapted/AppDelegate.swift
@@ -9,10 +9,36 @@ import UIKit
 import adadapted_swift_sdk
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, AaSdkSessionListener, AaSdkEventListener, AaSdkAdditContentListener {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        AdAdapted
+            .withAppId(key: "7D58810X6333241C")
+            .inEnv(env: AdAdapted.Env.DEV)
+            .setSdkSessionListener(listener: self)
+            .setSdkEventListener(listener: self)
+            .setSdkAdditContentListener(listener: self)
+            .start()
+        
+        //AdAdaptedListManager.itemAddedToList(item: "stuff", list: "list")
+        
+        
         return true
+    }
+    
+    func onHasAdsToServe(hasAds: Bool, availableZoneIds: Array<String>) {
+        print("Has Ads To Serve: \(hasAds)")
+        print("The following zones have ads to serve: \(availableZoneIds)")
+    }
+    
+    func onNextAdEvent(zoneId: String, eventType: String) {
+        print("Ad \(eventType) for Zone \(zoneId)")
+    }
+    
+    func onContentAvailable(content: AddToListContent) {
+        let listItems = content.getItems()
+        content.acknowledge()
+        print(String(format: "%d item(s) added to Default List", listItems.count))
     }
 
     // MARK: UISceneSession Lifecycle
@@ -28,7 +54,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
