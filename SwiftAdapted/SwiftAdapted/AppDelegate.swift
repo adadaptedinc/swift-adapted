993edf9e277a793135aab7823de4943b4968e4cf
Brett Clifton <bclifton@adadapted.com>
2024-02-13 10:59:51 -0500

    feat: debug logging

--- SwiftAdapted/SwiftAdapted/AppDelegate.swift
+++ SwiftAdapted/SwiftAdapted/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AaSdkSessionListener, AaS
             .inEnv(env: AdAdapted.Env.DEV)
             .enableKeywordIntercept(value: true)
             .enablePayloads(value: true)
+            .enableDebugLogging()
             .setSdkSessionListener(listener: self)
             .setSdkEventListener(listener: self)
             .setSdkAdditContentListener(listener: self)
